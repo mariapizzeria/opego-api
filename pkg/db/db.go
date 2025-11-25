5d802e17b9e5b109902583ac88697dcc403229b4
MariaPizzeria <107207681+mariapizzeria@users.noreply.github.com>
2025-11-25 17:19:46 +0500

    Добавление тестовой бд

---
+++ pkg/db/db.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	"github.com/mariapizzeria/opego-api/pkg/configs"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Db struct {
+	*gorm.DB
+}
+
+func NewDb(conf *configs.Config) *Db {
+	db, err := gorm.Open(postgres.Open(conf.Db.Dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return &Db{db}
+}
